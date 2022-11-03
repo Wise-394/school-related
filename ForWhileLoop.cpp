dc4c52d40ef4c2bcdb0a0d572de5e922b880aa87
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2022-11-04 05:22:48 +0800

    Create ForWhileLoop.cpp

---
+++ ForWhileLoop.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+	
+    //while loop
+	int i = 0;
+	while(i<5)
+	{
+		printf("%d",i);
+		i++;
+	}
+		printf("\n");
+		
+	//for loop
+	for(int j = 0; j < 5; j++)
+	{
+	    printf("%d",j);
+	}
+
+	
+	
+}
